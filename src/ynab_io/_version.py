f2732bde195d92319d0b881358d8a8514ac94913
Ilya <atamur@gmail.com>
2025-08-31 19:56:07 +0200

    temp commit before removing

--- src/ynab_io/_version.py
+++ src/ynab_io/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev1+g16020227e.d20250831'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g16020227e.d20250831')
+__version__ = version = '0.1.dev13+gf749e2aa0.d20250831'
+__version_tuple__ = version_tuple = (0, 1, 'dev13', 'gf749e2aa0.d20250831')
 
-__commit_id__ = commit_id = 'g16020227e'
+__commit_id__ = commit_id = 'gf749e2aa0'
